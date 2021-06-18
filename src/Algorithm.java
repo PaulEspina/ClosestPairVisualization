7513cb6a67a6a228e749c58199d828c3266f0938
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-06-19 00:51:01 +0800

    implemented the algorithm

---
+++ src/Algorithm.java
@@ -0,0 +1,88 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Algorithm
+{
+    public static double solve(Point[] p, Point[] q,  ArrayList<Point[]> subPoints)
+    {
+        if(p.length <= 3)
+        {
+            return bruteForce(p);
+        }
+
+        int middle = p.length / 2;
+        Point middlePoint = p[middle];
+        Point[] leftPoints = Arrays.copyOfRange(p, 0, p.length / 2);
+        Point[] rightPoints = Arrays.copyOfRange(p, p.length / 2, p.length);
+
+        double dl = solve(leftPoints, q, subPoints);
+        double dr = solve(rightPoints, q, subPoints);
+        double d = Math.min(dl, dr);
+
+        ArrayList<Point> stripP = new ArrayList<>();
+        ArrayList<Point> stripQ = new ArrayList<>();
+        for(int i = 0; i < p.length; i++)
+        {
+            if(Math.abs(p[i].x - middlePoint.x) < d)
+            {
+                stripP.add(p[i]);
+            }
+            if(Math.abs(q[i].x - middlePoint.x) < d)
+            {
+                stripQ.add(q[i]);
+            }
+        }
+
+        Point[] staticStripP = new Point[stripP.size()];
+        for(int i = 0; i < stripP.size(); i++)
+        {
+            staticStripP[i] = stripP.get(i);
+        }
+        Point[] staticStripQ = new Point[stripQ.size()];
+        for(int i = 0; i < stripQ.size(); i++)
+        {
+            staticStripQ[i] = stripQ.get(i);
+        }
+        QuickSort.sort(staticStripQ, 1);
+
+        double minA = Math.min(d, stripClosest(staticStripP, d));
+        double minB = Math.min(d, stripClosest(staticStripQ, d));
+
+        return Math.min(minA, minB);
+    }
+
+    private static double stripClosest(Point[] strip, double d)
+    {
+        double minVal = d;
+        for(int i = 0; i < strip.length; i++)
+        {
+            for(int j = i + 1; j < strip.length && (strip[j].y - strip[i].y) < minVal; j++)
+            {
+                minVal = dist(strip[i], strip[j]);
+            }
+        }
+        return minVal;
+    }
+
+    private static double bruteForce(Point[] points)
+    {
+        double minDistance = Double.MAX_VALUE;
+        for(int i = 0; i < points.length; i++)
+        {
+            for(int j = i + 1; j < points.length; j++)
+            {
+                if(dist(points[i], points[j]) < minDistance)
+                {
+                    minDistance = dist(points[i], points[j]);
+                }
+            }
+        }
+        return minDistance;
+    }
+
+    private static double dist(Point p1, Point p2)
+    {
+        return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+    }
+}
