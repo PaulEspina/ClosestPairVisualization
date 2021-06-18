7a902618886b33a8fecaa21be09b9a6c79f87871
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-06-18 22:24:00 +0800

    Graphical visualization set up

--- src/ClosestPair.java
+++ src/ClosestPair.java
@@ -1,4 +1,115 @@
+import java.awt.*;
+import java.awt.image.BufferStrategy;
 
-public class ClosestPair
+public class ClosestPair implements Runnable
 {
+    private final String TITLE;
+    private final int WIDTH, HEIGHT;
+
+    private Boolean running;
+    private GraphicDisplay graphicDisplay;
+    private BufferStrategy bufferStrategy;
+    private Graphics graphics;
+    private Thread thread;
+
+    public ClosestPair(String title, int width, int height)
+    {
+        TITLE = title;
+        WIDTH = width;
+        HEIGHT = height;
+        running = false;
+        graphicDisplay = null;
+        bufferStrategy = null;
+        graphics = null;
+        thread = null;
+    }
+
+    private void init()
+    {
+        graphicDisplay = new GraphicDisplay(TITLE, WIDTH, HEIGHT);
+
+
+    }
+
+    private void update()
+    {
+
+    }
+
+    private void render()
+    {
+        bufferStrategy = graphicDisplay.getCanvas().getBufferStrategy();
+        if(bufferStrategy == null)
+        {
+            graphicDisplay.getCanvas().createBufferStrategy(3);
+            return;
+        }
+
+        graphics = bufferStrategy.getDrawGraphics();
+        graphics.clearRect(0, 0, WIDTH, HEIGHT);
+
+        // START DRAW
+
+        // END DRAW
+
+        bufferStrategy.show();
+        graphics.dispose();
+    }
+
+    @Override
+    public void run()
+    {
+        init();
+
+        int fps = 60;
+        double timePerTick = 1000000000 / (double) fps;
+        double delta = 0;
+        long now;
+        long lastTime = System.nanoTime();
+        long timer = 0;
+        while(running)
+        {
+            now = System.nanoTime();
+            delta += (now - lastTime) / timePerTick;
+            timer += now - lastTime;
+            lastTime = now;
+            if(delta >= 1)
+            {
+                update();
+                render();
+                delta--;
+            }
+            if(timer >= 1000000000)
+            {
+                timer = 0;
+            }
+        }
+    }
+
+    public synchronized void start()
+    {
+        if(running)
+        {
+            return;
+        }
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void stop()
+    {
+        if(!running)
+        {
+            return;
+        }
+        try
+        {
+            thread.join();
+        }
+        catch(InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ClosestPair closestPair = new ClosestPair("Closest Pair Visualization", 600, 500);
+        closestPair.start();
+    }
+}
