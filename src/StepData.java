30c45b49d2a5772fac23fd19fdae01c399713c53
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-06-19 09:02:17 +0800

    created StepData struct
    This struct will help the data per step in the algorithm.

---
+++ src/StepData.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+
+public class StepData
+{
+    public int middle;
+    public Point middlePoint;
+    public Point[] leftPoints;
+    public Point[] rightPoints;
+    public double dl;
+    public double dr;
+    public double d;
+    public Point[] stripP;
+    public Point[] stripQ;
+    public double minA;
+    public double minB;
+    public double min;
+
+    public StepData(int middle, Point middlePoint, Point[] leftPoints, Point[] rightPoints, double dl, double dr, double d, Point[] stripP, Point[] stripQ, double minA, double minB, double min)
+    {
+        this.middle = middle;
+        this.middlePoint = middlePoint;
+        this.leftPoints = leftPoints;
+        this.rightPoints = rightPoints;
+        this.dl = dl;
+        this.dr = dr;
+        this.d = d;
+        this.stripP = stripP;
+        this. stripQ = stripQ;
+        this.minA = minA;
+        this.minB = minB;
+        this.min = min;
+    }
+}
